d297733f979843af55c527131103a1e97e5454ae
Igor.K <igor.k.nsk@gmail.com>
2023-10-26 17:51:57 +0700

    Add response to server customer

--- carsRouter.js
+++ carsRouter.js
@@ -1,12 +1,22 @@
-// /all_cars?park_id=3
-import getCarsData from './carsData.js'
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
+const fetch = require('node-fetch');
+
+// /all_cars?park_id=3
+
+// import getCarsData from './carsData.js'
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.json(getCarsData()).end()
+router.get('/', async (req, res) => {
+
+  const goToServerCustomer = await fetch('http://89.108.99.163/gps/gpsapi.php/all_cars?park_id=1')
+  const result = await goToServerCustomer.json()
+  console.log("▶ ⇛ result:", result);
+
+
+  res.json(result).end()
 });
 
 export default router;
