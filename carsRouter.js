cd6a138b2ca36cfa9ed740cec518b06786c196b8
Igor.K <igor.k.nsk@gmail.com>
2023-11-29 16:22:29 +0700

    add parametr park_id

--- carsRouter.js
+++ carsRouter.js
@@ -11,14 +11,22 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
 
-  const goToServerCustomer = await fetch('http://89.108.99.163/gps/gpsapi.php/all_cars?park_id=1')
+  const park_id = req.query?.park_id || 1
+  try {
+    const goToServerCustomer = await fetch(`http://89.108.99.163/gps/gpsapi.php/all_cars?park_id=${park_id}`)
   const result = await goToServerCustomer.json()
+    res.status(200).json(result).end()
+
+  } catch (error) {
+    res.status(500).end()
+
+  }
+
 
   // result.cars.map(el => el.pic = `https://89.108.99.163/pics/car2.png`)
   // console.log("▶ ⇛ result:", result);
 
 
-  res.json(result).end()
 });
 
 export default router;
